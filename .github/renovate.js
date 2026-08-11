04d5dc433d2bd19f91c629e86c10c57d05dd373b
Matthieu ALLIE <allie.matthieu@gmail.com>
2026-08-11 18:25:19 +0200

    ci(renovate): add renovate GHA

---
+++ .github/renovate.js
@@ -0,0 +1,8 @@
+module.exports = {
+    branchPrefix: 'chore/',
+    username: 'mallie-su',
+    gitAuthor: "Renovate <allie.matthieu@gmail.com>",
+    onboarding: true,
+    platform: 'github',
+    forkProcessing: 'false',
+  };
